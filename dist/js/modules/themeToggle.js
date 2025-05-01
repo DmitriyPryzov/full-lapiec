fa31a0dec102afd9cbd3b4df74720f6ca2ca840d
dmitriypryzov <79713892+DmitriyPryzov@users.noreply.github.com>
2025-05-01 21:30:57 +0300

    Delete dist directory

--- dist/js/modules/themeToggle.js
+++
@@ -1,5 +0,0 @@
- export default function toggleTheme(element) {
-    const body = document.body;
-    const isLight = body.classList.toggle('light-theme');
-    element.textContent = isLight ? '🌞' : '🌙';
-  }
